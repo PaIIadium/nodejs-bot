03cafd73575ee4c44e9f67577a69f08245eefc34
haiduchyk <42899572+haiduchyk@users.noreply.github.com>
2019-01-18 01:03:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,44 @@
 'use strict';
-
 const TelegramBot = require('node-telegram-bot-api');
-const vm = require('vm');
 
-const token = '700792770:AAHsyUZ-1vhIjdgW6RY7NMIg3WMQLkluIck';
+const { exec } = require('child_process');
+
+const token = '796127956:AAGBAY5LWVMU4JtnVWs878tlaT8i6_bF4Fw';
 
 const bot = new TelegramBot(token, { polling: true });
 
-async function compile(code) {
-  const sandbox = {
-    console: {
-      log(...a) {
-        sandbox.__res__.push(...a);
-      },
-      dir(...a) {
-        sandbox.__res__.push(...a);
-      },
-      info(...a) {
-        sandbox.__res__.push(...a);
-      },
-      error(a) {
-        sandbox.__res__.push(...a);
-      },
-    },
-    __res__: []
-  };
+function rate(code) {
+  if (!code) return 0;
+  return Math.floor((Math.random() * 60) + 40);
+}
 
-  const context = vm.createContext(sandbox);
-  try {
-    const script = new vm.Script(code);
-    script.runInContext(context);
-  } catch (error) {
-    sandbox.__res__.push('Сэр, ваше дерьмо не работает!');
-    sandbox.__res__.push(error.toString());
-  }
-  return sandbox.__res__;
+function repl(arg) {
+  return `'${arg.replace(/'/g, '\'\\\'\'')}'`;
 }
 
 bot.onText(/node (.+)/, (msg, match) => {
-  let count = 100;
   const fromId = msg.chat.id;
-  setTimeout(()=>{
-  bot.sendMessage(fromId, 'Гавнокод компилируется...');
-  },0);
-  const code = match[1];
-  (async ()=> {
-  const res = await compile(code);
-  for (const value of res) {
-    count += 100;
-   setTimeout(()=>{
-    bot.sendMessage(fromId, value);
-   },count) ;
-  }
-    setTimeout(()=>{
-   bot.sendMessage(fromId, 'End');
-  },count + 50);
-  })();
-  
-});
+  setTimeout(() => {
+    bot.sendMessage(fromId, 'Показывайте код...');
+  }, 0);
+  const code = match.input.split('\n').join(' ').slice(4);
+  const fcode = repl(code);
+  const rating = rate(code);
+  exec(`node -e ${fcode}`, { timeout: 1000 }, (error, stdout, stderr) => {
+    if (stdout) {
+      bot.sendMessage(fromId, stdout);
+      if (!stderr) {
+        setTimeout(() => {
+          bot.sendMessage(fromId, 'Поставлю вам...' + '\n' + rating);
+        }, 500);
+      }
+    }
+    if (stderr) {
+      console.error(`exec error: ${error}`);
+      bot.sendMessage(fromId, 'Поздравляю, вы попали на допку ', stderr);
+      // return;
+    }
 
+  });
+
+});
