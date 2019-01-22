56ae48ae2dfbfba8760ce814bbe6ad571e4d36fc
haiduchyk <42899572+haiduchyk@users.noreply.github.com>
2019-01-22 11:08:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 'use strict';
-const TelegramBot = require('node-telegram-bot-api');
 
+process.env['NTBA_FIX_319'] = 1;
+
+const Bot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
+const http = require('http');
 
-const token = '796127956:AAGBAY5LWVMU4JtnVWs878tlaT8i6_bF4Fw';
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.send('it is running\n');
+}).listen(process.env.PORT || 5000);
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new Bot('796127956:AAGBAY5LWVMU4JtnVWs878tlaT8i6_bF4Fw', { polling: true });
+const sendMessage = bot.sendMessage.bind(bot);
 
-function rate(code) {
-  if (!code) return 0;
-  return Math.floor((Math.random() * 60) + 40);
-}
-
-function repl(arg) {
+function escapeShellArg(arg) {
   return `'${arg.replace(/'/g, '\'\\\'\'')}'`;
 }
 
-bot.onText(/node (.+)/, (msg, match) => {
-  const fromId = msg.chat.id;
-  setTimeout(() => {
-    bot.sendMessage(fromId, 'Показывайте код...');
-  }, 0);
-  const code = match.input.split('\n').join(' ').slice(4);
-  const fcode = repl(code);
-  const rating = rate(code);
-  exec(`node -e ${fcode}`, { timeout: 1000 }, (error, stdout, stderr) => {
-    if (stdout) {
-      bot.sendMessage(fromId, stdout);
-      if (!stderr) {
-        setTimeout(() => {
-          bot.sendMessage(fromId, 'Поставлю вам...' + '\n' + rating);
-        }, 500);
-      }
+function replying(msg, match) {
+  const userId = msg.chat.id;
+  sendMessage(userId, 'Блин, опять в этом говне разбираться...');
+  console.log('@' + msg.from.username + ': ' + match[1]);
+  const code = escapeShellArg(match[1]);
+  exec(`timeout 1s node -e ${code}`, { }, (error, stdout, stderr) => {
+    if (error && error.code === 1) sendMessage(userId, 'Паяльник мне в процессор...Что он написал?\n' + stderr);
+    else if (!stdout) sendMessage(userId, 'Мне лень это считать, застрелитесь');
+    else {
+      const arr = stdout.split('\n');
+      const count = arr.join('');
+      if (arr.length < 102 && count.length < 1001) sendMessage(userId, 'С ума сойти, оно даже скомпилировалось...\n' + arr.join('   |   ').slice(0, -4));
+      else sendMessage(userId, 'Флуд не пройдёт!');
     }
-    if (stderr) {
-      bot.sendMessage(fromId, 'Поздравляю, вы попали на допку ', stderr);
-    }
-
   });
+}
+bot.onText(/node (.+)/, replying);
 
-});
