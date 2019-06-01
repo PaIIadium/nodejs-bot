542372c2dd94d2be16d3caf84e899946eaf8754a
PaIIadium <vladislavkuchin2001@gmail.com>
2019-06-01 19:57:15 +0300

    Update

--- index.js
+++ index.js
@@ -4,84 +4,94 @@ process.env['NTBA_FIX_319'] = 1;
 
 const Bot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
-const token = '724668528:AAE4fo6fQ9_VV-6Lvrh4DfMnOyhhpA4_UfQ';
+const token = process.env.tg_token;
 const bot = new Bot(token, { polling: true });
-
 const sendMessage = bot.sendMessage.bind(bot);
 
 function escapeShellArg(match) {
   const reg = /\/\//;
-  const res = (match.input.slice(4)).split('\n').map(el => (el.match(reg) ? el.slice(0, el.indexOf('\/\/')) : el)).join(' ');
+  const res = match.split('\n').map(el => (el.match(reg) ? el.slice(0, el.indexOf('\/\/')) : el)).join(' ');
   return `'${res.replace(/'/g, '\'\\\'\'')}'`;
 }
 
-function replying(msg, match) {
-  const userId = msg.chat.id;
-  console.log('@' + msg.from.username + ': ' + match[1]);
-  const code = escapeShellArg(match);
-  exec(`timeout 1.3s node -e ${code}`, (error, stdout, stderr) => {
-    if (error && error.code) {
-      console.log(error);
-      if (error.code === 124) {
-        sendMessage(userId, '_Timed out_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-      } else {
-        const a = stderr.split('\n').reverse().filter((_) => _.indexOf('Error') === -1 ? 0 : 1);
-        sendMessage(userId, '\`\`\` '  + a[0] + '\`\`\`', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-      }
-    } else {
-      const arr = stdout.split('\n');
-      const characters = arr.join('');
-      const count = characters.length;
-      if (arr.length < 26 && count < 1001) {
-        const res = '\`\`\` ' + arr.join('\n') + '\`\`\`';
-        sendMessage(userId, res + '\n', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id  });
-      } else if (arr.length >= 26 && count < 1001) {
-        const res = '\`\`\` ' + arr.slice(0, 51).join('\n') + '\`\`\`';
-        sendMessage(userId, res + '  _...Флуд_' + '\n', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-      } else if (arr.length < 26 && count >= 1001) {
-        let res = '';
-        for (const value of arr) {
-          if ((res + value).length < 1001) {
-            res += value + '\n';
+function replying(msg) {
+  if (msg.entities[0].type === 'bot_command' && msg.entities[0].offset === 0) {
+    const command = msg.text.match(/\/[a-z]+/)[0];
+    console.log(command);
+    if (command === '/node') {
+      const match = msg.text.slice(command.length);
+      const userId = msg.chat.id; 
+      console.log('@' + msg.from.username + ':' + match);
+      const code = escapeShellArg(match);
+      exec(`echo ${code} | timeout 1.5s node`, (error, stdout, stderr) => {
+        if (error && error.code) {
+          if (error.code === 124) {
+            sendMessage(userId, '_Timed out_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
           } else {
-            res += value.slice(1001 - res.length);
-            break;
-          }
-        }
-        if (res) res = '\`\`\` ' + res + '\`\`\`';
-        sendMessage(userId, res + '  _...Флуд_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-      } else if (arr.length >= 26 && count >= 1001) {
-        const sliceArr = arr.slice(0, 25);
-        let res = '';
-        if (sliceArr.join('').length > 1001) {
-          for (const value of sliceArr) {
-            if ((res + value).length < 1001) {
-              res += value + '\n';
-            } else {
-              res += value.slice(1001 - res.length);
-              break;
-            }
+            const a = stderr.split('\n').reverse().filter((_) => _.indexOf('Error') === -1 ? 0 : 1);
+            sendMessage(userId, '\`\`\` '  + a[0] + '\`\`\`', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
           }
         } else {
-          res = sliceArr.join('\n');
+          const arr = stdout.split('\n');
+          const characters = arr.join('');
+          const count = characters.length;
+          if (arr.length < 26 && count < 1001) {
+            if (count === 0) {
+              sendMessage(userId, '_This code does not log anything_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id  });
+            } else {
+              const res = '\`\`\` ' + arr.join('\n') + '\`\`\`';
+              sendMessage(userId, res + '\n', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id  });
+            }
+            } else if (arr.length >= 26 && count < 1001) {
+            const res = '\`\`\` ' + arr.slice(0, 51).join('\n') + '\`\`\`';
+            sendMessage(userId, res + '  _...Флуд_' + '\n', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
+          } else if (arr.length < 26 && count >= 1001) {
+            let res = '';
+            for (const value of arr) {
+              if ((res + value).length < 1001) {
+                res += value + '\n';
+              } else {
+                res += value.slice(1001 - res.length);
+                break;
+              }
+            }
+            if (res) res = '\`\`\` ' + res + '\`\`\`';
+            sendMessage(userId, res + '  _...Флуд_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
+          } else if (arr.length >= 26 && count >= 1001) {
+            const sliceArr = arr.slice(0, 25);
+            let res = '';
+            if (sliceArr.join('').length > 1001) {
+              for (const value of sliceArr) {
+                if ((res + value).length < 1001) {
+                  res += value + '\n';
+                } else {
+                  res += value.slice(1001 - res.length);
+                  break;
+                }
+              }
+            } else {
+              res = sliceArr.join('\n');
+            }
+            res = '\`\`\` ' + res + '\`\`\`';
+            sendMessage(userId, res + '  _...Флуд_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
+          }
         }
-        res = '\`\`\` ' + res + '\`\`\`';
-        console.log('z1');
-        sendMessage(userId, res + '  _...Флуд_', { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
-      }
+      });
+    } else if (command === '/start') {
+      sendMessage(msg.chat.id, 'Use "node" to compile your code. For example:' + '\n' + 'node console.log(\'Ave\');' + '\n' + 'setTimeout(() => console.log(\'Marcus Aurelius\'), 500);' + '\n' + 'You have only 900 msc to compile, so use them wisely' + '\n' + 'Rules:' + '\n' + '1.Insert your code right after the keyword <node>' + '\n' + '2.Use semicolons!', { parse_mode: 'Markdown' });
     }
-  });
+  }
 }
 
 const fn = () => {
   let status = true;
-  const delay = (msg, match) => {
+  const delay = msg => {
     if (status) {
       status = false;
-      setTimeout(() => (status = true), 1300);
-      replying(msg, match);
+      setTimeout(() => (status = true), 1500);
+      replying(msg);
     } else {
-      setTimeout(delay, 300, msg, match);
+      setTimeout(delay, 300, msg);
     }
   };
   return delay;
@@ -89,6 +99,4 @@ const fn = () => {
 
 const delay = fn();
 
-bot.onText(/node (.+)/, delay);
-
-bot.onText(/\/start/, msg => sendMessage(msg.chat.id, 'Use "node" to compile your code. For example:' + '\n' + 'node console.log(\'Ave\');' + '\n' + 'setTimeout(() => console.log(\'Marcus Aurelius\'), 500);' + '\n' + 'You have only 900 msc to compile, so use them wisely' + '\n' + 'Rules:' + '\n' + '1.Insert your code right after the keyword <node>' + '\n' + '2.Use semicolons!', { parse_mode: 'Markdown' }));
+bot.on('text', delay);
