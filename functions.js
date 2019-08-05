6d7c6dd3edf8c3caf75ce5501675a1c6574de929
PaIIadium <vladislavkuchin2001@gmail.com>
2019-08-05 17:25:47 +0300

    .

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@
 
 const bot = require('./bot');
 const fs = require('fs');
-const adminId = parseInt(fs.readFileSync('/root/bot/nodejs-bot/data/admin_id', 'utf8'), 10);
+const adminId = parseInt(fs.readFileSync('./data/admin_id', 'utf8'), 10);
 
 const escShellArg = match => {
   const reg = /\/\//;
