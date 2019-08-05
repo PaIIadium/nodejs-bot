583d7750758af430ee46cc61d619efc21bf1307e
PaIIadium <vladislavkuchin2001@gmail.com>
2019-08-05 17:24:28 +0300

    .

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 
 const Bot = require('node-telegram-bot-api');
 const fs = require('fs');
-const token = fs.readFileSync('/root/bot/nodejs-bot/data/tg_token', 'utf8').trim();
+const token = fs.readFileSync('./data/tg_token', 'utf8').trim();
 const bot = new Bot(token, { polling: true });
 
 module.exports = bot;
